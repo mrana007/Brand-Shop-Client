d14ea773944b362128428b700b667e8c43628e4f
mrana007 <rana.es.bc@gmail.com>
2023-10-18 01:18:47 +0600

    added AuthProvider to use Usecontext

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,82 @@
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FcGoogle } from "react-icons/fc";
+import swal from 'sweetalert';
+
+const Login = () => {
+    const[showPassword, setShowPassword] = useState(false)
+    const {login, signInWithGoogle} = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        login(email, password)
+        .then(result => {
+            console.log(result.user);
+            swal("Login successfully", "", "success");
+            navigate(location?.state ? location.state : '/');
+
+        })
+        .catch(error =>{
+            
+           console.error(error);
+           swal("Invalid user information", "", "warning");
+        })
+    }
+
+    const handleGoogleSignIn =() =>{
+        signInWithGoogle()
+        .then(result =>{
+            console.log(result.user);
+            navigate(location?.state ? location.state : '/');
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+    }
+
+    return (
+        <div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl mx-auto mb-8" data-aos="fade-up">
+            <h2 className="text-3xl my-6 text-center font-bold">Login your account</h2>
+            <form onSubmit={handleLogin} className="w-3/4 mx-auto">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <div className="relative input input-bordered">
+            <input type={showPassword ? "text" : "password"} name="password" placeholder="password" className="pt-2" required />
+            <span className="absolute top-3 right-3" onClick={ () => setShowPassword(!showPassword)}>
+                {showPassword ? <FaEyeSlash/> : <FaEye/>}
+            </span>
+            </div>
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-fuchsia-700 normal-case text-white font-semibold text-2xl">Login</button>
+        </div>
+            </form>
+        <div className="form-control pt-4 px-12">
+          <button onClick={handleGoogleSignIn} className="btn btn-outline text-fuchsia-600 normal-case font-semibold text-2xl"><FcGoogle/> Login with Google</button>
+        </div>
+            <p className="text-center mt-4 pb-4">Do not Have An Account ? <Link to='/register' className="font-extrabold text-blue-600">Register</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,100 @@
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import {  updateProfile } from "firebase/auth";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import swal from 'sweetalert';
+
+
+
+const Register = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const {createUser, logOut} = useContext(AuthContext);
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+
+        const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+        if(!passwordValidation.test(password)){
+            swal("Password required minimum six characters, at least one uppercase and one lowercase letter and one special (@$!%*?&) character", "", "error");
+            return;
+        }
+
+        // create user
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user);
+            swal("User created successfully!", "", "success");
+            logOut();
+
+            // update profile
+            updateProfile(result.user,{
+                displayName: name,
+                photoURL: photo
+            })
+            .then(result =>{
+                console.log(result.user);
+            })
+            .catch(error =>{
+                console.error(error);  
+            })
+
+        })
+        .catch(error => {
+            console.error(error);
+            swal("User already registered", "", "warning");
+        })
+    }
+
+    return (
+        <div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-fuchsia-200 mx-auto mb-8" data-aos="fade-down">
+                <h2 className="text-3xl my-6 text-center font-bold">Register your account</h2>
+            <form onSubmit={handleRegister} className="w-3/4 mx-auto">
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Your Name</span>
+                </label>
+                    <input type="text" name="name" placeholder="Enter your name" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Your Photo</span>
+                </label>
+                    <input type="text" name="photo" placeholder="Enter your photo url" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                </div>
+            <div className="form-control">
+            <label className="label">
+                <span className="label-text">Password</span>
+            </label>
+            <div className="relative input input-bordered">
+            <input type={showPassword ? "text" : "password"} name="password" placeholder="password" className="pt-2" required />
+            <span className="absolute top-3 right-3" onClick={ () => setShowPassword(!showPassword)}>
+                {showPassword ? <FaEyeSlash/> : <FaEye/>}
+            </span>
+            </div>
+            </div>
+            <div className="form-control mt-6">
+            <button className="btn bg-fuchsia-700 normal-case text-white font-semibold text-2xl">Register</button>
+            </div>
+            </form>
+            <p className="text-center mt-4 pb-4">Already Have An Account ? <Link to='/login' className="font-extrabold text-blue-600">Login</Link>
+            </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
